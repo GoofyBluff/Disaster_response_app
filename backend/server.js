5b672af05d3ba2159358a4fefcac9482366122c0
Prashant Kumar <kumarprashantggn@gmail.com>
2025-10-08 16:26:16 +0530

    mongoDB Connected

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Connected to MongoDB"))
+.catch((error) => console.error("MongoDB connection error ", error));
+
+
+app.get('/', (req, res) => {
+  res.send("Disaster response App is running")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+})
